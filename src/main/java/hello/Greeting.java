4967f25d8bcc3ae9bda35fa2ad4cac16cf5c8609
Adam Kassai <adamkassai@gmail.com>
2018-03-02 16:58:03 +0100

    Spring Boot Application

---
+++ src/main/java/hello/Greeting.java
@@ -0,0 +1,24 @@
+public class Greeting {
+
+    private final long id;
+    private final String content;
+    private final String alma = "alma";
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getAlma() {
+        return alma;
+    }
+
+}
