297315d14e7e85383ffca25bdcd76f633adc0d82
Adam Kassai <adamkassai@gmail.com>
2018-03-03 17:28:34 +0100

    Adding rabbitTemplate to LobbyController

--- src/main/java/com/bestinpest/controller/LobbyController.java
+++ src/main/java/com/bestinpest/controller/LobbyController.java
@@ -2,6 +2,7 @@ package com.bestinpest.controller;
 
 import com.bestinpest.model.Lobby;
 import com.bestinpest.repository.LobbyRepository;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +15,12 @@ public class LobbyController {
     @Autowired
     LobbyRepository lobbyRepository;
 
+    @Autowired
+    RabbitTemplate rabbitTemplate;
+
     @RequestMapping("/lobbies")
     public List<Lobby> lobbies() {
+        rabbitTemplate.convertAndSend("bip-exchange", "", "Hello from RabbitMQ!");
         return lobbyRepository.findAll();
     }
 
