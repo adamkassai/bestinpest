c3fbcd170688034d11124bec6e21a856990b00fa
Adam Kassai <adamkassai@gmail.com>
2018-03-03 12:25:30 +0100

    RabbitMQ configuration and example code

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -2,6 +2,7 @@ package hello;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,6 +12,11 @@ import org.springframework.context.annotation.Bean;
 public class Application {
 
     private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange("bip-exchange");
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
