702fdcfd08acd5d28dab6d700d93e016b06cdf95
Adam Kassai <adamkassai@gmail.com>
2018-03-25 15:43:28 +0200

    Fix in ApplicationController

--- src/main/java/com/bestinpest/controller/ApplicationController.java
+++ src/main/java/com/bestinpest/controller/ApplicationController.java
@@ -3,12 +3,15 @@ package com.bestinpest.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class ApplicationController {
 
     @GetMapping("/rabbitmq-rx-url")
-    public String rabbitmqRxUrl() {
-        return System.getenv("RABBITMQ_BIGWIG_RX_URL");
+    public Map<String, String> rabbitmqRxUrl() {
+        return new HashMap<String, String>(){{put("url", System.getenv("RABBITMQ_BIGWIG_RX_URL"));}};
     }
 
 }
