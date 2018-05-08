4658ee3d32f74f1027143c39bf4e989211940e7a
Adam Kassai <adamkassai@gmail.com>
2018-05-08 14:26:12 +0200

    Bugfix in FutarToDB

--- src/main/java/com/bestinpest/config/FutarToDB.java
+++ src/main/java/com/bestinpest/config/FutarToDB.java
@@ -42,15 +42,14 @@ public class FutarToDB implements CommandLineRunner {
     public void run(String... args) {
 
         String response = restTemplate.getForObject(
-                "http://futar.bkk.hu/bkk-utvonaltervezo-api/ws/otp/api/where/stops-for-location.json?lat=47.497638&lon=19.053021&radius="+gameConfig.getCityRadius(), String.class);
+                "http://futar.bkk.hu/bkk-utvonaltervezo-api/ws/otp/api/where/stops-for-location.json?lat=47.497638&lon=19.053021&radius=" + gameConfig.getCityRadius(), String.class);
 
         JsonParser parser = new JsonParser();
         JsonArray stopsArray = parser.parse(response).getAsJsonObject().getAsJsonObject("data").getAsJsonArray("list");
 
         List<String> routeList = new ArrayList<>();
 
-        for (JsonElement stopElement : stopsArray)
-        {
+        for (JsonElement stopElement : stopsArray) {
             JsonObject stopObject = stopElement.getAsJsonObject();
             String id = stopObject.get("id").getAsString();
             String name = stopObject.get("name").getAsString();
@@ -60,7 +59,7 @@ public class FutarToDB implements CommandLineRunner {
 
             JsonArray routesArray = stopObject.get("routeIds").getAsJsonArray();
 
-            if (routesArray.size()>0) {
+            if (routesArray.size() > 0) {
 
                 Stop stop = new Stop(id, name, lat, lon);
 
@@ -98,10 +97,9 @@ public class FutarToDB implements CommandLineRunner {
         Map<String, Route> routes = new HashMap<>();
 
 
-        for (String routeId : routeList)
-        {
+        for (String routeId : routeList) {
             response = restTemplate.getForObject(
-                    "http://futar.bkk.hu/bkk-utvonaltervezo-api/ws/otp/api/where/route-details.json?routeId="+routeId+"&related=false", String.class);
+                    "http://futar.bkk.hu/bkk-utvonaltervezo-api/ws/otp/api/where/route-details.json?routeId=" + routeId + "&related=false", String.class);
 
             JsonObject entry = parser.parse(response).getAsJsonObject().getAsJsonObject("data").getAsJsonObject("entry");
             String relationName = entry.get("shortName").getAsString();
@@ -111,33 +109,28 @@ public class FutarToDB implements CommandLineRunner {
             String backgroundColor = entry.get("color").getAsString();
             JsonArray variantsArray = entry.getAsJsonArray("variants");
 
-            for (JsonElement variantElement: variantsArray)
-            {
+            for (JsonElement variantElement : variantsArray) {
                 JsonObject variant = variantElement.getAsJsonObject();
                 String headsign = variant.get("headsign").getAsString();
                 JsonArray stopIds = variant.get("stopIds").getAsJsonArray();
 
-                for (int i=0; i<stopIds.size(); i++)
-                {
+                for (int i = 0; i < stopIds.size(); i++) {
                     String stopId = stopIds.get(i).getAsString();
                     Optional<Stop> stop = stopRepository.findById(stopId);
 
                     if (stop.isPresent()) {
 
-                        int end = i+1;
-                        for (int j=i+1; j<=end && j<stopIds.size(); j++)
-                        {
+                        int end = i + 1;
+                        for (int j = i + 1; j <= end && j < stopIds.size(); j++) {
                             String terminalStopId = stopIds.get(j).getAsString();
                             Optional<Stop> terminalStop = stopRepository.findById(terminalStopId);
 
-                            if (terminalStop.isPresent())
-                            {
-                                String id = stop.get().getId()+terminalStop.get().getId()+type;
+                            if (terminalStop.isPresent()) {
+                                String id = stop.get().getId() + terminalStop.get().getId() + type;
                                 Route route;
-                                if (routes.containsKey(id))
-                                {
+                                if (routes.containsKey(id)) {
                                     route = routes.get(id);
-                                }else{
+                                } else {
                                     route = routeRepository.save(new Route(stop.get(), terminalStop.get(), type));
                                     routes.put(id, route);
                                 }
@@ -145,7 +138,7 @@ public class FutarToDB implements CommandLineRunner {
                                 Relation relation = relationRepository.save(new Relation(relationId, relationName, headsign, textColor, backgroundColor, route));
                                 route.getRelations().add(relation);
                                 routeRepository.save(route);
-                            }else{
+                            } else {
                                 end++;
                             }
 
@@ -160,33 +153,44 @@ public class FutarToDB implements CommandLineRunner {
         }
 
         List<Junction> junctions = junctionRepository.findAll();
-        String type="WALK";
+        String type = "WALK";
 
-        for(Junction j1 : junctions) {
+        for (int i = 0; i < junctions.size(); i++) {
 
-            for (Junction j2: junctions) {
+            for (int j = 0; j < junctions.size(); j++) {
 
-            if (routeService.distance(j1.getStops().get(0).getLat(), j2.getStops().get(0).getLat(), j1.getStops().get(0).getLon(), j2.getStops().get(0).getLon())<=gameConfig.getMaxWalkDistance()
-                    && routeService.getRoutesBetween(j1.getId(), j2.getId()).isEmpty()) {
+                Junction j1 = junctions.get(i);
+                Junction j2 = junctions.get(j);
 
 
-                String id = j1.getStops().get(0).getId()+j2.getStops().get(0).getId()+type;
-                Route route;
-                if (!routes.containsKey(id))
-                {
-                    route = routeRepository.save(new Route(j1.getStops().get(0), j2.getStops().get(0), type));
-                    routes.put(id, route);
+                if (!j1.equals(j2) && j1.getStops().size() > 0 && j2.getStops().size() > 0) {
 
-                    Relation relation = relationRepository.save(new Relation(id, "WALK", j2.getName(), "#FFFFFF", "#000000", route));
-                    route.getRelations().add(relation);
-                    routeRepository.save(route);
+                    Stop s1 = j1.getStops().get(0);
+                    Stop s2 = j2.getStops().get(0);
+
+
+                    if (routeService.distance(s1.getLat(), s2.getLat(), s1.getLon(), s2.getLon()) <= gameConfig.getMaxWalkDistance()
+                            && routeService.getRoutesBetween(j1.getId(), j2.getId()).isEmpty()) {
+
+                        String id = s1.getId() + s2.getId() + type;
+                        Route route;
+                        if (!routes.containsKey(id)) {
+                            route = routeRepository.save(new Route(s1, s2, type));
+                            routes.put(id, route);
+
+                            Relation relation = relationRepository.save(new Relation(id, "WALK", j2.getName(), "#FFFFFF", "#000000", route));
+                            route.getRelations().add(relation);
+                            routeRepository.save(route);
+                        }
+
+
+                    }
+
                 }
-
-
-            }
 
             }
         }
+
 
     }
 }
